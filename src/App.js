4b382f73e2950448d683286991b5c195c95061fe
hegphp <98257593+hegphp@users.noreply.github.com>
2023-11-20 11:39:56 +0700

    Created the first customer components!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as util from "./components/utils.js";
+
+function Header() {
+  return (
+    <header className="App-header App">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Xin chào! Đây là chương trình ReactJs đầu tiên của tôi!</p>
+      <p>Utils variables: ${util.default}</p>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"><Header/></div>;
 }
 
 export default App;
---
+++ src/components/utils.js
@@ -0,0 +1,4 @@
+export let variable1 = "This is the variable 1";
+export let variable2 = "This is the variable 2";
+const defaultvar = "This is the default variable";
+export default defaultvar;
