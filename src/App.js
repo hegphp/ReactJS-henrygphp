d6bfd3e954bec88133ae0a8a9db5d10731cf573e
hegphp <98257593+hegphp@users.noreply.github.com>
2023-11-22 23:11:55 +0700

    Created dynamic values

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import * as util from "./components/utils.js";
 
+const reactDesc = ["Core", "Fundamental", "Crucial"];
+
+function getRandInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
   return (
     <header className="App-header App">
       <img src={logo} className="App-logo" alt="logo" />
       <p>Xin chào! Đây là chương trình ReactJs đầu tiên của tôi!</p>
       <p>Utils variables: ${util.default}</p>
+      {/* Dòng dưới đây gọi giá trị của hàm getRandInt() */}
+      <p>Dynamics Random variable: {reactDesc[getRandInt(2)]}</p>
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
@@ -24,7 +32,11 @@ function Header() {
 }
 
 function App() {
-  return <div className="App"><Header/></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
